320597e3839229f8a6ecd3cb03b825027179d5e7
Anton <anton.gusevv.01@gmail.com>
2023-06-25 20:53:00 +0300

    build: конфигурация devServer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: ['vuetify'],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7204',
+      },
+    },
+  },
 });
